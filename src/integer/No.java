78f3d47413d0d9ab8c1de980a93151418b27c2cc
guhrabelo <gustavo.rabelo.teles01@gmail.com>
2021-11-26 19:34:22 -0300

    first commit

---
+++ src/integer/No.java
@@ -0,0 +1,11 @@
+package integer;
+
+public class No {
+	int dado;
+	No proximo;
+
+	@Override
+	public String toString() {
+		return "Dado: " + dado;
+	}
+}
---
+++ src/string/No.java
@@ -0,0 +1,11 @@
+package string;
+
+public class No {
+	String dado;
+	No proximo;
+
+	@Override
+	public String toString() {
+		return "Dado: " + dado;
+	}
+}
